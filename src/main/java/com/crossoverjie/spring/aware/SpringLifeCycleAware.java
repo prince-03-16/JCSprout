68ac43e795aafedabcdfdbd0ca83895427b07dad
Pratikrocks <pratikrocks.dey11@gmail.com>
2020-06-15 01:01:28 +0530

    trying
    Signed-off-by: Pratikrocks <pratikrocks.dey11@gmail.com>

--- src/main/java/com/crossoverjie/spring/aware/SpringLifeCycleAware.java
+++ src/main/java/com/crossoverjie/spring/aware/SpringLifeCycleAware.java
@@ -8,13 +8,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
-/**
- * Function:
- *
- * @author crossoverJie
- *         Date: 20/03/2018 21:54
- * @since JDK 1.8
- */
 @Component
 public class SpringLifeCycleAware implements ApplicationContextAware {
     private final static Logger LOGGER = LoggerFactory.getLogger(SpringLifeCycleAware.class);
